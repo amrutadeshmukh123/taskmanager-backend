6fbc171ab0a87e09dafc6b72a65615a297bfcb66
Amruta Deshmukh <deshmukhamruta394@gmail.com>
2025-03-05 23:17:05 +0530

    update

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ server.use(bodyParser.json())
 server.use('/api',userRoutes)
 
 
-mongoose.connect('mongodb+srv://amruta:i6GGEm8Mi8cZYaZ8@test2.zh7cv.mongodb.net/',{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
+mongoose.connect('mongodb+srv://amruta:i6GGEm8Mi8cZYaZ8@test2.zh7cv.mongodb.net/api/',{
+    // useNewUrlParser:true,
+    // useUnifiedTopology:true
 }).then(()=>{
     console.log("DB connected !!")
 }).catch(()=>{
